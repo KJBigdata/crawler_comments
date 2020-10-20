114ee976c7a01d1c6e63d8d75994d130a0818456
JIHYO <kj.bigdata@gmail.com>
2020-10-20 14:43:22 +0900

    edit remark in base

--- crawler/utils/base.py
+++ crawler/utils/base.py
@@ -6,12 +6,29 @@ from datetime import datetime, timedelta
 import pickle
 
 def get_all_date(start_date, end_date):
+    """It retuns all dates between start date and end date
+
+    Args:
+      start date : start point to calculate date #
+      end date : end point to calculate date #
+
+    To use:
+    >>> get_all_date(start_date, end_date)
+    """
     d1 = datetime.strptime(start_date, '%Y%m%d').date()
     d2 = datetime.strptime(end_date, '%Y%m%d').date()
     date_list = [str(d1 + timedelta(days=x)).replace('-', '') for x in range((d2 - d1).days + 1)]
     return date_list
 
 def get_url_list(main_url):
+    """It retuns url list of several news in main url
+
+    Args:
+      main_url : url with combination of code, date and page
+
+    To use:
+    >>> get_url_list(main_url)
+    """
     ua = UserAgent()
     headers = {'User-Agent': ua.random}
     main_url = requests.get(main_url, headers=headers)
@@ -24,6 +41,14 @@ def get_url_list(main_url):
 
 
 def remove_duplicate(title_url_pair_list=list):
+    """It retuns url list with removed duplicate case
+
+    Args:
+      title_url_pair_list : list of (title, url) tuples
+
+    To use:
+    >>> remove(title_url_pair_list)
+    """
     my_set = set()
     res = []
     for title, url in title_url_pair_list:
@@ -32,15 +57,6 @@ def remove_duplicate(title_url_pair_list=list):
             my_set.add(url)
 
     return res
-
-def load_comments(fname):
-    with open(fname, encoding='utf-8') as f:
-        docs = [doc.strip() for doc in f]
-    if docs[0][-15:] != 'antipathy_count':
-        first = docs[0].split('antipathy_count')[0] + 'antipathy_count'
-        second = docs[0].split('antipathy_count')[1]
-        docs = [first, second] + docs[1:]
-    return docs
 
 def mkdir(path: str) -> None:
     """Recursively creates the directory and does not raise an exception
@@ -54,10 +70,18 @@ def mkdir(path: str) -> None:
     os.makedirs(path, exist_ok=True)
 
 def load_docs(abs_path):
+    """load pickle
+
+    Args:
+      path: A target directory path
+
+    To use:
+    >>> load_docs('target_directory_path')
+    """
     try:
         with open(abs_path, 'rb') as f:
             whole_doc = pickle.load(f)
-            return whole_doc
+        return whole_doc
     except Exception as ex:
         print(ex)
 
@@ -74,9 +98,17 @@ def dump_docs(crawled_data, output_dir: str, file_name: str):
     path = os.path.join(output_dir, f"{file_name}.pkl")
     with open(path, 'wb') as file:
         pickle.dump(crawled_data, file)
-    show_stat(path, file_name)
+    show_stat(path)
 
-def show_stat(abs_path, file_name):
+def show_stat(abs_path):
+    """It show count of news&comments after crawling
+
+    Args:
+      abs_path: A target directory path
+
+    To use:
+    >>> show_stat('target_directory_path')
+    """
     doc_len = 0
     comment_len = 0
     with open(abs_path, 'rb') as f:
@@ -85,7 +117,7 @@ def show_stat(abs_path, file_name):
         doc_len += 1
         for comment in doc.get('comments'):
             comment_len +=1
-    print(f"file name : {file_name}")
+    print(f"file : {abs_path}")
     print(f"crawled_news_n : {doc_len}")
     print(f"crawled_comment_n : {comment_len}")
     print('\n')
